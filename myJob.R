820656ab1336f28b52dccbe4e94814ce8de93a74
jtomiyama <jtomiyama@uiowa.edu>
2022-04-19 00:26:04 -0500

    Switch optim package.

--- myJob.R
+++ myJob.R
@@ -65,7 +65,7 @@ if(length(step_params) > 0){
 mspec <- ModelSpecification(rec, 
                             model = models[[opt$model]],
                             control = ctrl) %>%
-  set_optim_bayes()
+  set_optim_bayes("rBayesianOptimization")
 
 template_list <- list("rec" = rec,
      'mspec' = mspec,
